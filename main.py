b25361ad8766c3deb0adbaa5368a90037f893cab
NODA, Kai <nodakai@gmail.com>
2018-07-09 00:46:11 +0800

    appveyor.py runs main.py as a test
    See rust-lang/rust 51816

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+from __future__ import print_function
 import os
 import logging
 import threading
 import time
+import contextlib
+import shutil
+
+
+@contextlib.contextmanager
+def output(filepath):
+    tmp = filepath + '.tmp'
+    with open(tmp, 'w') as f:
+        yield f
+    try:
+        os.replace(tmp, filepath)
+    except AttributeError:
+        try:
+            os.rename(tmp, filepath)
+        except OSError:
+            try:
+                os.remove(filepath)
+                os.rename(tmp, filepath)
+            except PermissionError:
+                shutil.copy2(tmp, filepath)
+                os.remove(tmp)
 
 
 def read_open_kernel(path):
     logging.info("opening %s...", path)
-    with open(path) as fp:
+    with open(path) as _:
         time.sleep(1.)
     logging.info("closed %s", path)
 
@@ -14,16 +36,20 @@ def read_open_kernel(path):
 def test01():
     path = 'hello.txt'
     with open(path, 'wt') as fp:
-        fp.write('hello\n')
+        print('hello', file=fp)
     logging.info("created %s", path)
 
     read_open = threading.Thread(target=read_open_kernel, args=(path,))
     read_open.start()
     try:
-        time.sleep(0.1)
-        logging.info("trying to remove %s", path)
-        os.remove(path)
-        logging.info("successfully removed %s", path)
+        time.sleep(0.5)
+        logging.info("trying to apply output() to %s", path)
+        with output(path) as fp:
+            print('goodbye', file=fp)
+        logging.info("successfully returned from open()")
+        with open(path) as fp:
+            dat = fp.read(999)
+            logging.info("now it contains %r", dat)
     except OSError as ex:
         logging.warning("got %r", ex, exc_info=True)
     finally:
